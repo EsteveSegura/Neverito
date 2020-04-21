91b7e9a38901508f4a864ed17b1e343e37087ee4
estevesegura <estevesegura@gmail.com>
2020-04-21 04:06:31 +0200

    quick test

--- src/backend/server.js
+++ src/backend/server.js
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(cors());
 app.use(session({
-    secret: process.env.SESSION_KEY || 'supersecret',
+    secret: process.env.SESSION_KEY || 'supersecret1',
     resave: true,
     saveUninitialized: true,
 }))
